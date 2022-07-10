48af5a93f0c558e3c3850fb9793c9db8a3a205d6
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-10 11:31:25 +0800

    Create multiplyNumeric() function

---
+++ object-multiply-numeric/script.js
@@ -0,0 +1,15 @@
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+function multiplyNumeric(object){
+  for (let key in object) {
+    if (typeof object[key] == 'number') object[key] = object[key] * 2;
+  }
+}
+
+console.log(menu);
