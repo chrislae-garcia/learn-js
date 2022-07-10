e1887e36d9365e8e3ed9ca3badccdbbca39a8664
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-10 10:32:52 +0800

    Create isEmpty() function

--- object-isempty/script.js
+++ object-isempty/script.js
@@ -5,3 +5,11 @@ alert( isEmpty(schedule) ); // true
 schedule["8:30"] = "get up";
 
 alert( isEmpty(schedule) ); // false
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+  return true;
+}
